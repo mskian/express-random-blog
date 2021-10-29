0720194359ee5eaf6f77357f7686e09d0f2d8646
mskian <veerfun23@gmail.com>
2021-10-29 07:04:46 +0530

    add empty text validation for markdown outputs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const matter = require('gray-matter');
-const md = require("markdown-it")();
+const md = require('markdown-it')();
 const fs = require('fs')
-const apicache = require("apicache");
+const apicache = require('apicache');
 
 const app = express();
 const port = 4002;
@@ -17,6 +17,16 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+function getCurrentDate(n) {
+    return (n < 10 ? "0" : "") + n
+  }
+
+const date = new Date()
+const month = getCurrentDate(date.getMonth() + 1)
+const day = getCurrentDate(date.getDate())
+const year = date.getFullYear()
+const formattedDate = year + "-" + month + "-" + day
+
 app.listen(port, function() {
     console.log('listening on port ' + port);
 });
@@ -27,11 +37,11 @@ app.get('/', cache('1 hour'), function(req, res) {
     res.header('X-XSS-Protection', '1; mode=block');
     res.header('X-Content-Type-Options', 'nosniff');
     res.header('Strict-Transport-Security', 'max-age=63072000');
-    res.render("index")
+    res.render('index')
 
 });
 
-app.get("/:article", cache('1 hour'), (req, res) => {
+app.get('/:article', cache('1 hour'), (req, res) => {
 
     res.header('X-Frame-Options', 'DENY');
     res.header('X-XSS-Protection', '1; mode=block');
@@ -44,18 +54,18 @@ app.get("/:article", cache('1 hour'), (req, res) => {
         const file = matter.read(__dirname + '/blog/content/' + req.params.article + '.md');
         const content = file.content;
         const result = md.render(content);
-        res.render("blog", {
-            post: result,
-            title: file.data.title,
-            description: file.data.description,
-            date: file.data.date,
-            seourl: req.params.article
+        res.render('blog', {
+            post: result || 'Hello Word Post Content Here',
+            title: file.data.title || 'Post title Comes Here',
+            description: file.data.description || 'Hello World - Post title description Here',
+            date: file.data.date || formattedDate,
+            seourl: req.params.article || ''
         });
     } else {
-        res.render("404");
+        res.render('404');
     }
 });
 
 app.use('/', function(req, res) {
-    res.render("404");
+    res.render('404');
 });
